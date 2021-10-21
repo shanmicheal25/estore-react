51050f6516fe9fa54aee376fa58e3b47def605ac
shankar <shanmicheal25@gmail.com>
2021-10-21 13:40:21 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import Header from "./components/Header";
+import "./App.css";
+import MainContainer from "./components/MainContainer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <MainContainer />
+    </>
+  );
+}
+
+export default App;
